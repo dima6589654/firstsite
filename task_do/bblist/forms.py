acc55580e53f614effe0ecdb86b3a8575594439d
Kan Dima <kandima334@gmai.com>
2023-09-29 01:28:06 +0600

    home_work_37

---
+++ task_do/bblist/forms.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from captcha.fields import CaptchaField
+from django import forms
+from django.core.validators import FileExtensionValidator
+
+from bblist.models import IceCream
+from .models import Task
+
+
+class TaskForm(forms.ModelForm):
+    # captcha = CaptchaField()
+
+    class Meta:
+        model = Task
+        fields = ('title', 'description', 'attachment', 'priority', 'due_date')
+        widgets = {
+            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+
+    attachment = forms.FileField(
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=['pdf', 'xlsx', 'gif', 'jpg', 'png', 'doc', 'txt', 'zip'],
+                message='Поддерживаются только файлы PDF, XLSX, GIF, JPG, PNG, DOC, TXT и ZIP.',
+
+            )
+        ]
+    )
+
+
+class IceCreamForm(forms.ModelForm):
+    class Meta:
+        model = IceCream
+        fields = ('flavor', 'topping', 'price')
+
+
+class SearchForm(forms.Form):
+    keyword = forms.CharField(max_length=20, label="Поиск")
---
+++ task_do/bblist/views.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+
+from .forms import SearchForm
+from .forms import TaskForm, IceCreamForm
+from .models import Task, IceCream
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
+from django.utils import timezone
+from .forms import TaskForm, IceCreamForm, SearchForm
+from .models import Task, IceCream
+
+
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+
+    return render(request, 'create_task.html', {'form': form})
+
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_detail', task_id=task_id)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'edit_task.html', {'form': form, 'task': task})
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'delete_task.html', {'task': task})
+
+
+def task_list(request):
+    tasks = Task.objects.all()
+    paginator = Paginator(tasks, 5)
+    page = request.GET.get('page')
+    tasks = paginator.get_page(page)
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    return render(request, 'task_detail.html', {'task': task})
+
+
+def create_icecream(request):
+    if request.method == 'POST':
+        form = IceCreamForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('icecream_list')
+    else:
+        form = IceCreamForm()
+    return render(request, 'create_icecream.html', {'form': form})
+
+
+def icecream_list(request):
+    icecream = IceCream.objects.all()
+    paginator = Paginator(icecream, 5)
+    page = request.GET.get('page')
+    icecream = paginator.get_page(page)
+    return render(request, 'icecream_list.html', {'icecream': icecream})
+
+
+def tasks_due_today(request):
+    current_datetime = timezone.now()
+    tasks_due_today1 = Task.get_task_by_due_date(current_datetime.date())
+    return render(request, 'tasks_due_today.html', {'tasks_due_today': tasks_due_today1})
+
+
+def tasks_ordered_by_due_date(request):
+    tasks_ordered = Task.get_tasks_ordered_by_due_date()
+    return render(request, 'tasks_ordered_by_due_date.html', {'tasks_ordered': tasks_ordered})
+
+
+def task_titles(request):
+    tasks = Task.objects.values('title', 'priority')
+    return render(request, 'task_titles.html', {'tasks': tasks})
+
+
+def search(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            keyword = form.cleaned_data['keyword']
+            tasks = Task.objects.filter(title__icontains=keyword)
+            return render(request, 'search_results.html', {'tasks': tasks, 'keyword': keyword})
+    else:
+        form = SearchForm()
+    return render(request, 'search.html', {'form': form})
+
+
+def search(request):
+    if request.method == "POST":
+        sf = SearchForm(request.POST)
+        if sf.is_valid():
+            keyword = sf.cleaned_data['keyword']
+            bbs = Task.objects.filter(title__iregex=keyword, )
+            context = {'bbs': bbs, "form": sf}
+            return render(request, 'search_results.html', context)
+    else:
+        sf = SearchForm()
+    context = {'form': sf}
+    return render(request, 'search.html', context)

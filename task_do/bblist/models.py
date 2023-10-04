81f3f055843fdad550e743968796fd48d6fde99d
Kan Dima <kandima334@gmai.com>
2023-10-04 22:15:14 +0600

    home_work_36

---
+++ task_do/bblist/models.py
@@ -0,0 +1,41 @@
+import os
+
+from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Описание")
+    priority = models.IntegerField(verbose_name="Приоритет")
+    due_date = models.DateTimeField(verbose_name="Срок выполнения")
+    attachment = models.FileField(verbose_name="Вложение", upload_to='attachments/', default='default_attachment.jpg')
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def get_task_by_due_date(cls, due_date):
+        return cls.objects.filter(due_date=due_date)
+
+    @classmethod
+    def get_tasks_ordered_by_due_date(cls):
+        return cls.objects.order_by('due_date')
+
+
+# Функция для удаления файла при удалении задачи
+@receiver(post_delete, sender=Task)
+def delete_attachment(sender, instance, **kwargs):
+    if instance.attachment:
+        if os.path.isfile(instance.attachment.path):
+            os.remove(instance.attachment.path)
+
+
+class IceCream(models.Model):
+    flavor = models.CharField(max_length=100, verbose_name="Вкус")
+    topping = models.CharField(max_length=100, verbose_name="Топпинг")
+    price = models.DecimalField(max_digits=10, decimal_places=1, verbose_name="Цена")
+
+    def __str__(self):
+        return f"{self.flavor} - {self.topping}"
